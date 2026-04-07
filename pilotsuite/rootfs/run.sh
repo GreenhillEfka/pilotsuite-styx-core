8c9863ffc916f963af29dda336254113bed1d478
Stxy <stxy@pilotsuite>
2026-04-07 12:44:50 +0200

    fix(build): add Dockerfile and build assets for pilotsuite core

---
+++ pilotsuite/rootfs/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/with-contenv bashio
+set -e
+
+bashio::log.info "Starting PilotSuite Core v1.0.0..."
+
+# Start PilotSuite Core (Platzhalter für reale Engine)
+python3 -m http.server 8909
