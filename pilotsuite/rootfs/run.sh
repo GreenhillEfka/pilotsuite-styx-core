a78bda7af568ad0ba5fe2352d158a3cb0f9d0317
Stxy <stxy@pilotsuite>
2026-04-07 13:47:03 +0200

    release: v1.0.0 Platinum Core Final

--- pilotsuite/rootfs/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/with-contenv bashio
-set -e
-bashio::log.info "Starting PilotSuite v100.0.0..."
-python3 /usr/src/app/main.py
---
+++ pilotsuite_core/rootfs/usr/src/app/main.py
@@ -0,0 +1,1 @@
+import time; print('PilotSuite Core v1.0.0 Platinum is live.'); time.sleep(3600)
